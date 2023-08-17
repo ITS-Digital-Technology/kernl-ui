8e37482b597ee96e57d22de06be24a418c5ad020
Cameron Van Orman <cameron.vanorman@gmail.com>
2023-08-17 16:42:05 -0700

    Update config/colors path

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -1,5 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('./config/colors.js')
+const colors = require('../config/colors.js')
 
 module.exports = {
   presets: [require('tailwindcss/defaultConfig')],
